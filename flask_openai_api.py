0eab5deaf6005ab35bd0f4440c1b85a9c6c0b1b7
EngAziz92 <eng.azizmahmoud92@gmail.com>
2024-12-14 16:42:09 +0100

    Bind Flask app to 0.0.0.0 and dynamic port

--- flask_openai_api.py
+++ flask_openai_api.py
@@ -51,4 +51,6 @@ def get_past_participle():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Bind to PORT environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)

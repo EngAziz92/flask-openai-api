0e1b0eb418211b168c55941e15d60e71dee700f6
EngAziz92 <eng.azizmahmoud92@gmail.com>
2024-12-18 12:00:02 +0100

    Update flask_openai_api.py

--- flask_openai_api.py
+++ flask_openai_api.py
@@ -52,5 +52,5 @@ def get_past_participle():
 # Run the app
 if __name__ == '__main__':
     # Bind to PORT environment variable or default to 5000
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port, debug=True)
